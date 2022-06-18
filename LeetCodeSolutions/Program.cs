ea297c7af133b1b3e992d98fb35c5872744bcf17
Chris Jones <chris24442@gmail.com>
2022-06-18 20:33:07 +0100

    add function template

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -12,6 +12,14 @@ namespace LeetCodeSolutions
         //13. Roman to Integer
         public int RomanToInt(string s)
         {
+            // create dict of letter values
+
+            // int solution = 0;
+            // iterate over the input string
+                // take LeftChar
+                // if RightChar > LeftChar
+                    // mustbe IV or something
+                // else add old char and move on
             return 0;
         }
     }

88cfc8525bce89e657975360bc5eb4801c89e8cf
Chris Jones <chris24442@gmail.com>
2022-06-19 01:19:23 +0100

    Add tests based upon errors for numberToWords

--- LeetCodeTests/NumberToWordsTests.cs
+++ LeetCodeTests/NumberToWordsTests.cs
@@ -36,5 +36,45 @@ namespace LeetCodeTests
 
             Assert.AreEqual("One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven", result);
         }
+
+        [TestMethod]
+        public void NumberToWordsErrorTest1()
+        {
+            var program = new LeetCodeSolutions.Program();
+
+            string result = program.NumberToWords(0);
+
+            Assert.AreEqual("Zero", result);
+        }
+
+        [TestMethod]
+        public void NumberToWordsErrorTest2()
+        {
+            var program = new LeetCodeSolutions.Program();
+
+            string result = program.NumberToWords(100);
+
+            Assert.AreEqual("One Hundred", result);
+        }
+
+        [TestMethod]
+        public void NumberToWordsErrorTest3()
+        {
+            var program = new LeetCodeSolutions.Program();
+
+            string result = program.NumberToWords(1000);
+
+            Assert.AreEqual("One Thousand", result);
+        }
+
+        [TestMethod]
+        public void NumberToWordsErrorTest4()
+        {
+            var program = new LeetCodeSolutions.Program();
+
+            string result = program.NumberToWords(1000000);
+
+            Assert.AreEqual("One Million", result);
+        }
     }
 }

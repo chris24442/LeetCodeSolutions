377864c87afa1b8387daf0461cce8bc7047de7de
Chris Jones <chris24442@gmail.com>
2022-06-19 21:16:47 +0100

    Add autoResetEvent attempt

--- LeetCodeSolutions/PrintFooBarAlternatively.cs
+++ LeetCodeSolutions/PrintFooBarAlternatively.cs
@@ -3,20 +3,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace LeetCodeSolutions
 {
     public class PrintFooBarAlternatively
     {
 
+
         //1115. Print FooBar Alternately
         public class FooBar
         {
             private int n;
+            private Action printFoo = () => { Console.Write("foo"); };
+            private Action printBar = () => { Console.Write("bar"); };
+            //private static AutoResetEvent printFooBool = new AutoResetEvent(true);
+            private static AutoResetEvent print = new AutoResetEvent(true);
 
             public FooBar(int n)
             {
                 this.n = n;
+                Task.Factory.StartNew(() => Foo(printFoo));
+                Task.Factory.StartNew(() => Bar(printBar));
             }
 
             public void Foo(Action printFoo)
@@ -24,9 +32,10 @@ namespace LeetCodeSolutions
 
                 for (int i = 0; i < n; i++)
                 {
-
+                    print.WaitOne();
                     // printFoo() outputs "foo". Do not change or remove this line.
                     printFoo();
+                    print.Set();
                 }
             }
 
@@ -35,9 +44,10 @@ namespace LeetCodeSolutions
 
                 for (int i = 0; i < n; i++)
                 {
-
+                    print.WaitOne();
                     // printBar() outputs "bar". Do not change or remove this line.
                     printBar();
+                    print.Set();
                 }
             }
         }

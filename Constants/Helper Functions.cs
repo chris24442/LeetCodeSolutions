1cf11dade07a3c7ba8fe671d5897bbdc0ec362f5
Chris Jones <chris24442@gmail.com>
2022-06-19 01:19:03 +0100

    Creating rounding helper functions

---
+++ Constants/Helper Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Constants
+{
+    public static class HelperFunctions
+    {
+        public static int RoundUp(int toRound, int roundingMultiple)
+        {
+            if (toRound % roundingMultiple == 0) return toRound;
+            return (roundingMultiple - toRound % roundingMultiple) + toRound;
+        }
+
+        public static int RoundDown(int toRound, int roundingMultiple)
+        {
+            return toRound - toRound % roundingMultiple;
+        }
+    }
+
+}

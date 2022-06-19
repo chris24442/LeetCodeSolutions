be4c04cde097b2e457991a96e3a15f2f321fd801
Chris Jones <chris24442@gmail.com>
2022-06-19 20:27:56 +0100

    Committ example tests

---
+++ LeetCodeTests/PrintInOrderTests.cs
@@ -0,0 +1,57 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeSolutions;
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace LeetCodeTests
+{
+    [TestClass]
+    public class PrintInOrderTests
+    {
+        Action doWorkA = () => { Console.Write("First"); };
+        Action doWorkB = () => { Console.Write("Second"); };
+        Action doWorkC = () => { Console.Write("Third"); };
+
+        [TestMethod]
+        public void PrintInOrderExampleTest1()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                var a = new LeetCodeSolutions.Program.Foo();
+
+                Task.Factory.StartNew(() => a.First(doWorkA));
+                Task.Factory.StartNew(() => a.Second(doWorkB));
+                Thread.Sleep(50);
+                Task.Factory.StartNew(() => a.Third(doWorkC));
+                Thread.Sleep(50);
+
+                string expected = string.Format("FirstSecondThird");
+                Assert.AreEqual<string>(expected, sw.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void PrintInOrderExampleTest2()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                var a = new LeetCodeSolutions.Program.Foo();
+
+                Task.Factory.StartNew(() => a.First(doWorkA));
+                Task.Factory.StartNew(() => a.Third(doWorkC));
+                Thread.Sleep(50);
+                Task.Factory.StartNew(() => a.Second(doWorkB));
+                Thread.Sleep(50);
+
+                string expected = string.Format("FirstSecondThird");
+                Assert.AreEqual<string>(expected, sw.ToString());
+            }
+        }
+    }
+}

3b0a5b27ce588f6087313a7900ed9c623401499f
Chris Jones <chris24442@gmail.com>
2022-06-19 01:18:40 +0100

    add intToLongString Dictionary

--- Constants/Dictionaries.cs
+++ Constants/Dictionaries.cs
@@ -55,5 +55,51 @@ namespace Constants
             {2000, "MM"},
             {3000 , "MMM"},
         };
+
+        public static Dictionary<int, string> intToLongString = new Dictionary<int, string>()
+        {
+            {0, "Zero"},
+            {1, "One"},
+            {2, "Two"},
+            {3, "Three"},
+            {4, "Four"},
+            {5, "Five" },
+            {6, "Six"},
+            {7, "Seven"},
+            {8 , "Eight"},
+            {9, "Nine"},
+            {10, "Ten" },
+            {11, "Eleven"},
+            {12, "Twelve"},
+            {13, "Thirteen"},
+            {14, "Fourteen"},
+            {15, "Fifteen"},
+            {16, "Sixteen"},
+            {17, "Seventeen" },
+            {18, "Eighteen"},
+            {19, "Nineteen"},
+            {20, "Twenty"},
+            {30, "Thirty"},
+            {40, "Forty"},
+            {50, "Fifty"},
+            {60, "Sixty"},
+            {70, "Seventy"},
+            {80, "Eighty"},
+            {90, "Ninety"},
+            {100, "Hundred"},
+            {1000, "Thousand"},
+            {1000000, "Million"},
+            {1000000000, "Billion"}
+            // TODO: something clever with 64 bit int
+            //{1000000000000, "Trillion"},
+            //{1000000000000000, "Quadrillion"},
+            //{1000000000000000000, "Quintillion"},
+            //{1000000000000000000, "Nonillion"},
+            //{1000000000000000000000, "Decillion"}
+//Googol: 1 followed by 100 zeros
+//Centillion: 1 followed by 303 zeros
+//Googolplex: 1 followed by a googol zero  s
+        };
+
     }
 }

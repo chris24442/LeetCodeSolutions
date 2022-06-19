06f83e3f90dba96fcff998f24f4317245dba03be
Chris Jones <chris24442@gmail.com>
2022-06-19 20:28:16 +0100

    committ autoResetEvent solution

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace LeetCodeSolutions
 {
@@ -13,6 +14,47 @@ namespace LeetCodeSolutions
         }
 
         #region 19JUNE22
+
+        // 1114. Print in Order
+
+        public class Foo
+        {
+            // https://docs.microsoft.com/en-us/dotnet/api/system.threading.autoresetevent?view=net-6.0
+            // true is the signalled state, false is unsignalled
+            private static AutoResetEvent event_1 = new AutoResetEvent(false);
+            private static AutoResetEvent event_2 = new AutoResetEvent(false);
+            //private static AutoResetEvent event_3 = new AutoResetEvent(false); // not needed
+
+            public Foo()
+            { }
+
+            public void First(Action printFirst)
+            {
+                // printFirst() outputs "first". Do not change or remove this line.
+                printFirst();
+                event_1.Set();
+            }
+
+            public void Second(Action printSecond)
+            {
+                event_1.WaitOne();
+                // printSecond() outputs "second". Do not change or remove this line.
+                printSecond();
+                event_2.Set();
+            }
+
+            public void Third(Action printThird)
+            {
+                event_2.WaitOne();
+                // printThird() outputs "third". Do not change or remove this line.
+                printThird();
+            }
+        }
+
+
+        #endregion
+
+        #region 18JUNE22
 
         // 13. Roman to Integer
         // Feedback after looking at hints: Would be better to iterate right to left
